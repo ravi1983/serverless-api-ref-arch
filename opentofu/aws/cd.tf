ca31085521d787f0e84785f0e4ac2d0f705e5add
Ravi Shankar <shankaravi24@gmail.com>
2025-12-28 19:49:01 -0600

    Kickoff codedeploy to deploy lambda
    Signed-off-by: Ravi Shankar <shankaravi24@gmail.com>

---
+++ opentofu/aws/cd.tf
@@ -0,0 +1,46 @@
+resource "aws_codedeploy_app" "cart_app" {
+  compute_platform = "Lambda"
+  name             = "cart-function-deploy"
+}
+
+resource "aws_codedeploy_deployment_group" "cart_dg" {
+  app_name               = aws_codedeploy_app.cart_app.name
+  deployment_group_name  = "cart-production"
+  deployment_config_name = "CodeDeployDefault.LambdaAllAtOnce" # Shifts 100% immediately
+  service_role_arn       = aws_iam_role.codedeploy_role.arn
+
+  deployment_style {
+    deployment_option = "WITH_TRAFFIC_CONTROL"
+    deployment_type   = "BLUE_GREEN"
+  }
+}
+
+resource "aws_iam_role" "codedeploy_role" {
+  name = "cart-lambda-codedeploy-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "codedeploy.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codedeploy_lambda" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRoleForLambda"
+  role       = aws_iam_role.codedeploy_role.name
+}
+
+resource "aws_lambda_alias" "cart_alias" {
+  name             = "live"
+  function_name    = aws_lambda_function.cart_function.function_name
+  function_version = aws_lambda_function.cart_function.version
+
+  lifecycle {
+    ignore_changes = [function_version]
+  }
+}
--- opentofu/aws/compute.tf
+++ opentofu/aws/compute.tf
@@ -12,26 +12,85 @@ resource "aws_lambda_layer_version" "db_layer" {
 }
 
 # Cart function
-module "cart_function" {
-  source  = "terraform-aws-modules/lambda/aws"
-
+resource "aws_lambda_function" "cart_function" {
   function_name = "cart-function"
-  handler = "cart_handler.lambda_handler"
-  runtime = "python3.12"
+  handler       = "cart_handler.lambda_handler"
+  runtime       = "python3.12"
+  role          = aws_iam_role.lambda_exec_role.arn
+
+  filename      = "${path.module}/dummy.zip"
   layers = [aws_lambda_layer_version.db_layer.arn]
 
-  create_package = false
-  local_existing_package = "${path.module}/dummy.zip"
-  ignore_source_code_hash = true
-
-  vpc_subnet_ids = module.serverless-vpc.private_subnets
-  vpc_security_group_ids = [module.lambda_sg.security_group_id]
-  attach_network_policy = true
-
-  environment_variables = {
-    DATABASE_URL = module.item-catalog-db.db_instance_address
-    CART_TABLE_NAME = module.serverless-dynamodb-cart.dynamodb_table_id
+  vpc_config {
+    subnet_ids         = module.serverless-vpc.private_subnets
+    security_group_ids = [module.lambda_sg.security_group_id]
   }
+
+  environment {
+    variables = {
+      DATABASE_URL    = module.item-catalog-db.db_instance_address
+      CART_TABLE_NAME = module.serverless-dynamodb-cart.dynamodb_table_id
+    }
+  }
+
+  # THIS NOW WORKS because it is a resource, not a module
+  lifecycle {
+    ignore_changes = [
+      layers,
+      filename,
+      source_code_hash
+    ]
+  }
+}
+
+# The "Assume Role" Policy (allows Lambda to use this role)
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "cart-function-exec-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
+  })
+}
+
+# The DynamoDB Read Policy
+resource "aws_iam_role_policy" "dynamodb_read" {
+  name = "dynamodb-read-policy"
+  role = aws_iam_role.lambda_exec_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = [
+        "dynamodb:GetItem",
+        "dynamodb:Query",
+        "dynamodb:Scan",
+        "dynamodb:BatchGetItem"
+      ]
+      Resource = [module.serverless-dynamodb-cart.dynamodb_table_arn]
+    },
+      {
+        # Necessary for Lambda to run inside a VPC
+        Effect = "Allow"
+        Action = [
+          "ec2:CreateNetworkInterface",
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:DeleteNetworkInterface"
+        ]
+        Resource = "*"
+      }]
+  })
+}
+
+# Standard CloudWatch logging permission
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 module "lambda_sg" {
