62184b638e44a4306527cb1bf9504854eea0c877
Ravi Shankar <shankaravi24@gmail.com>
2025-12-29 21:16:44 -0600

    Removed password logging
    Signed-off-by: Ravi Shankar <shankaravi24@gmail.com>

--- serverless/db_layer/db.py
+++ serverless/db_layer/db.py
@@ -4,18 +4,17 @@ import boto3
 import json
 
 def get_cart_table():
-    CART_TABLE = os.environ.get('CART_TABLE_NAME', 'UserCarts')
+    cart_table = os.environ.get('CART_TABLE_NAME', 'UserCarts')
 
     endpoint = os.environ.get('DYNAMODB_ENDPOINT') # For integration testing
     print(f'Endpoint got from env is {endpoint}')
     dynamodb = boto3.resource('dynamodb')
-    return dynamodb.Table(CART_TABLE)
+    return dynamodb.Table(cart_table)
 
 secret_arn = os.environ['DB_SECRET_ARN']
 if secret_arn:
     secretsmanager = boto3.client('secretsmanager')
     db_creds = secretsmanager.get_secret_value(SecretId=secret_arn)
-    print(f'********************Database creds is {db_creds}')
 
 def get_psql_connection():
     """Returns a connection to the RDS Postgres instance."""
