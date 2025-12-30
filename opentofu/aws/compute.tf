d39c95b36881a52e0325251f6d61d09afc580f12
Ravi Shankar <shankaravi24@gmail.com>
2025-12-30 16:16:15 -0600

    Enable S3 hosting of cart app
    Signed-off-by: Ravi Shankar <shankaravi24@gmail.com>

--- opentofu/aws/compute.tf
+++ opentofu/aws/compute.tf
@@ -216,3 +216,47 @@ resource "aws_lambda_permission" "api_gw" {
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_apigatewayv2_api.cart_api.execution_arn}/*/*"
 }
+
+# Static website hosting
+resource "aws_s3_bucket" "static_site" {
+  bucket = "cart-app-768238794"
+}
+
+resource "aws_s3_bucket_website_configuration" "static_site_config" {
+  bucket = aws_s3_bucket.static_site.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "allow_public" {
+  bucket = aws_s3_bucket.static_site.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "public_read_policy" {
+  depends_on = [aws_s3_bucket_public_access_block.allow_public]
+
+  bucket = aws_s3_bucket.static_site.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.static_site.arn}/*"
+      },
+    ]
+  })
+}
